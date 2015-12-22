dbfa87d8d67bd705c99890d3bd020457a1b5c9d4
tesmen <tezmo@mail.ru>
2015-12-23 01:11:20 +0300

    C:: fix

--- src/AppBundle/C.php
+++ src/AppBundle/C.php
@@ -7,4 +7,32 @@ class C
     const AD_BUY = 1;
     const AD_SELL = 2;
     const AD_EXCHANGE = 3;
+
+    const SP = 'serializedParams';
+    const AD_ID = 'adId';
+    const VPS_P_APPROVAL_CODE = 'approvalCode';
+    const FORM_DOMAIN = 'domain';
+    const FORM_TARIFF = 'tariff';
+    const FORM_PROLONG_PERIODS = 'prolongPeriods';
+    const FORM_BUTTONS_LIST = 'button_list';
+    const FORM_AUTOPROLONG = 'autoprolong';
+    const FORM_AUTOPERIOD = 'autoperiod';
+    const FORM_TARIFF_LIST = 'tariff_list';
+    const FORM_PANELS_LIST = 'panels_list';
+    const FORM_OS = 'os';
+    const FORM_OS_ID = 'os_id';
+    const FORM_VIP = 'vip';
+    const FORM_PANEL = 'panel';
+    const FORM_BUTTON = 'button';
+    const FORM_PERIOD = 'period';
+    const FORM_BACKUP = 'backup';
+    const FORM_PROMOCODE = 'promoCode';
+    const FORM_IS_TRIAL = 'is_trial';
+    const FORM_SUPPORT = 'support';
+    const FORM_CATEGORY = 'category';
+    const FORM_PRODUCT = 'product';
+    const FORM_IMPORTANCE = 'importance';
+    const FORM_SUBJECT = 'subject';
+    const FORM_TEXT = 'text';
+    const FORM_ATTACH = 'attach';
 }
