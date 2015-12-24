49be1d21ff4edb3b17b558da27413a70ec216d24
tesmen <tezmo@mail.ru>
2015-12-25 02:00:47 +0300

    partsVendor fields

--- src/AppBundle/Entity/PartsVendor.php
+++ src/AppBundle/Entity/PartsVendor.php
@@ -18,7 +18,22 @@ class PartsVendor
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
- 
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $parent;
+
+
     /**
      * Get id
      *
@@ -27,5 +42,53 @@ class PartsVendor
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return PartsVendor
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param integer $parent
+     *
+     * @return PartsVendor
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return integer
+     */
+    public function getParent()
+    {
+        return $this->parent;
     }
 }
