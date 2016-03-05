e47b1ac6806656fb0df753897191d806e62e73bc
tesmen <tezmo@mail.ru>
2016-03-05 21:20:37 +0300

    moved default controller

--- src/VelovitoBundle/Controller/DefaultController.php
+++ src/VelovitoBundle/Controller/DefaultController.php
@@ -2,12 +2,29 @@
 
 namespace VelovitoBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function velovitoAction(Request $request)
     {
-        return $this->render('VelovitoBundle:Default:index.html.twig');
+        return $this->render(
+            'VelovitoBundle:default:velovito.html.twig',
+            [
+                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            ]
+        );
+    }
+
+    public function indexAction(Request $request)
+    {
+        return $this->render(
+            'VelovitoBundle:default:index.html.twig',
+            [
+                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            ]
+        );
     }
 }
