94d0f7b30b608e07ade3abc9257873963ff1550e
tesmen <tezmo@mail.ru>
2016-03-19 19:05:58 +0300

    DESC

--- src/VelovitoBundle/Model/Advertisement/AdvertisementModel.php
+++ src/VelovitoBundle/Model/Advertisement/AdvertisementModel.php
@@ -47,7 +47,7 @@ class AdvertisementModel
     public function getFewLastAds($limit = 15)
     {
         return $this->em->getRepository(C::REPO_ADVERTISEMENT)->findBy(
-            ['status' => 1],
+            [],
             ['creationDate' => 'DESC']
         );
     }
