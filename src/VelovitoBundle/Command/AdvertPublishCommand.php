79956e5830282f368340a33296bcfd49506c8f78
tesmen <sergey.t@artvisio.com>
2016-03-21 10:40:50 +0300

    AdvertPublishCommand

---
+++ src/VelovitoBundle/Command/AdvertPublishCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace VelovitoBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use VelovitoBundle\C;
+
+class AdvertPublishCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('advert:publish');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
+        $ents = $em->getRepository(C::REPO_ADVERTISEMENT)->findAll();
+        $c = 0;
+
+        foreach ($ents as $advert) {
+            $advert->setStatus(C::ADVERT_STATUS_PUBLISHED);
+            $c++;
+        }
+        $em->flush();
+
+        $output->writeln("Publuished $c adverts");
+
+    }
+}
