0cbff5ae956eb551bad2a7b4fc70c7958119d240
tesmen <tezmo@mail.ru>
2016-03-22 23:35:39 +0300

    GregwarImageService

--- src/VelovitoBundle/Service/GregwarImageService.php
+++ src/VelovitoBundle/Service/GregwarImageService.php
@@ -1,14 +1,29 @@
 <?php
 namespace VelovitoBundle\Service;
 
-use Gregwar;
 use VelovitoBundle\C;
+use Gregwar\Image\Image;
+use VelovitoBundle\Model\DefaultModel;
 
 class GregwarImageService
 {
-    public function __construct()
+    public function __construct(DefaultModel $defaultModel)
     {
-
+        $this->defaultModel = $defaultModel;
     }
 
+    public function getImage($filename)
+    {
+        $filePath = $this->defaultModel->getImageOriginalsDir($filename);
+
+        return new Image($filePath);
+    }
+
+    public function resize($fileName, $witdh, $height)
+    {
+        $image = $this->getImage($fileName);
+
+        return $image
+            ->scaleResize($witdh, $height);
+    }
 }
--- src/VelovitoBundle/Twig/GregwarImageExtension.php
+++ src/VelovitoBundle/Twig/GregwarImageExtension.php
@@ -2,18 +2,30 @@
 
 namespace VelovitoBundle\Twig;
 
+use Symfony\Component\Routing\Router;
+use VelovitoBundle\Service\GregwarImageService;
+
 class GregwarImageExtension extends \Twig_Extension
 {
+    public function __construct(GregwarImageService $gregwarImageService, Router $router)
+    {
+        $this->imageService = $gregwarImageService;
+        $this->router = $router;
+    }
+
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('image', [$this, 'paramsFilter']),
+            new \Twig_SimpleFilter('resize', [$this, 'resize']),
         ];
     }
 
-    public function paramsFilter($in, $width, $heigth)
+    public function resize($imageName, $width, $heigth)
     {
-        return 'image';
+        return 'http://'
+            .$this->router->getContext()->getHost()
+            .'/'
+            .$this->imageService->resize($imageName, $width, $heigth);
     }
 
     public function getName()
