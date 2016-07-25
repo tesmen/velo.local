abef16536ca8a30b889aa9b85e56d9cebdcd3349
tesmen <tezmo@mail.ru>
2016-07-25 17:59:18 +0300

    deprecates

--- src/VelovitoBundle/Repository/AdvertisementRepository.php
+++ src/VelovitoBundle/Repository/AdvertisementRepository.php
@@ -2,41 +2,10 @@
 
 namespace VelovitoBundle\Repository;
 
-use VelovitoBundle\Entity\Advertisement;
-use VelovitoBundle\Entity\UserPhoto;
-use VelovitoBundle\Entity\User;
-use VelovitoBundle\C;
 use VelovitoBundle\Exception\NotFoundException;
 
 class AdvertisementRepository extends GeneralRepository
 {
-    /**
-     * @param array $data
-     * @throws \Exception
-     * @deprecated
-     */
-    public function load(array $data)
-    {
-        $this->_em->beginTransaction();
-
-        try {
-            foreach ($data as $id => $roleName) {
-                $ent = new Advertisement();
-                $ent->setId($id);
-                $ent->setName($roleName);
-
-                $this->_em->persist($ent);
-            }
-
-            $this->truncateTable();
-            $this->_em->flush();
-            $this->_em->commit();
-        } catch (\Exception $e) {
-            $this->_em->rollback();
-            throw $e;
-        }
-    }
-
     /**
      * @param $advertid
      * @return null|object
