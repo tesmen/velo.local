fba0c5ec7f793ebde4331e570cc49822cd024987
tesmen <tezmo@mail.ru>
2016-10-24 13:28:06 +0300

    test controller

---
+++ src/VelovitoBundle/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace VelovitoBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use VelovitoBundle\C;
+use VelovitoBundle\Form\User\UserProfileForm;
+
+class TestController extends GeneralController
+{
+    public function testAction(Request $request)
+    {
+        return $this->render(
+            'VelovitoBundle::test.html.twig',
+            [
+            ]
+        );
+    }
+}
