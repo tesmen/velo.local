48b88456009e184b36c5fda5666bbe4078f42bd7
tesmen <tezmo@mail.ru>
2016-10-27 11:11:50 +0300

    admin angular started

---
+++ web/js/admin-js/controllers.js
@@ -0,0 +1,43 @@
+angular.module('myApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "dashboard.html"
+            })
+            .when("/dashboard", {
+                templateUrl: "dashboard.html",
+                controller: "redCtrl",
+                reloadOnSearch: false
+            })
+            .when("/green", {
+                templateUrl: "green.htm"
+            })
+            .when("/blue", {
+                templateUrl: "blue.htm"
+            });
+    })
+    .controller('myCtrl', myCtrl)
+    .controller('redCtrl', redCtrl)
+    .filter('greet', greetFilter);
+
+function myCtrl($scope, $http) {
+    $scope.foo = 1;
+
+    $scope.getIt = function () {
+        $http.get('http://velo.local/api/search-adverts')
+            .success(
+                function (data) {
+                    console.log(data)
+                }
+            )
+    }
+}
+function redCtrl($scope) {
+    $scope.red = 'red';
+}
+
+function greetFilter() {
+    return function (name) {
+        return 'Hello, ' + name + '!';
+    }
+}
