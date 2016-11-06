774b6d72798557c9f39ae34af96b956c8372d51d
tesmen <tezmo@mail.ru>
2016-11-06 18:24:39 +0300

    update command fix

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+git reset HEAD
 git pull
 
 php5.6 bin/console assets:install
+php5.6 bin/console doc:sch_upd --force
 
 php5.6 bin/console cache:clear --env=prod
 php5.6 bin/console cache:clear --env=dev
