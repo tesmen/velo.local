7b32cd782cbb4c9c2877bac752e593ae5a3429bd
tesmen <tezmo@mail.ru>
2016-12-04 23:35:20 +0300

    markup fix

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git reset HEAD
 git pull
 
 php5.6 bin/console assets:install
-php5.6 bin/console doc:sch_upd --force
+php5.6 bin/console doc:sch:upd --force
 
 php5.6 bin/console cache:clear --env=prod
 php5.6 bin/console cache:clear --env=dev
